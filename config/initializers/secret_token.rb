a3d498cc519c35ecf0a8b138989a34365f0c6636
Diego Scataglini <dwebsubmit@gmail.com>
2013-10-08 09:34:48 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotornot::Application.config.secret_token = '3cb7aef6a21217b716242cec6862d928da236cba7eb5823a01404466dbcfc4aba81b9675b8204af9e33dbea169135f15a91fd11d80439551073be351e9a1da02'
